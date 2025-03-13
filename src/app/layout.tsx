46bcd652503f0a5d61b18d85c15ff3c606d817cf
Dhushyanth-h-m <dhushyanth.manjunath@gmail.com>
2025-03-12 21:08:45 -0500

    Refactor: Move GA ID to environment variables

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -8,8 +8,6 @@ const inter = Inter({
   variable: "--font-inter",
 });
 
-const GA_MEASUREMENT_ID = 'G-Q7WV1XKCWJ';
-
 export const metadata = {
   title: "Mac Frame Generator",
   description: "Transform your screenshots into beautiful MacBook mockups",
@@ -26,20 +24,24 @@ export default function RootLayout({
   return (
     <html lang="en">
       <head>
-        <Script
-          src={`https://www.googletagmanager.com/gtag/js?id=${GA_MEASUREMENT_ID}`}
-          strategy="afterInteractive"
-        />
-        <Script id="ga-script" strategy="afterInteractive">
-          {`
-            window.dataLayer = window.dataLayer || [];
-            function gtag(){dataLayer.push(arguments);}
-            gtag('js', new Date());
-            gtag('config', '${GA_MEASUREMENT_ID}', {
-              page_path: window.location.pathname,
-            });
-          `}
-        </Script>
+        {process.env.NEXT_PUBLIC_GA_ID && (
+          <>
+            <Script
+              src={`https://www.googletagmanager.com/gtag/js?id=${process.env.NEXT_PUBLIC_GA_ID}`}
+              strategy="afterInteractive"
+            />
+            <Script id="google-analytics" strategy="afterInteractive">
+              {`
+                window.dataLayer = window.dataLayer || [];
+                function gtag(){dataLayer.push(arguments);}
+                gtag('js', new Date());
+                gtag('config', '${process.env.NEXT_PUBLIC_GA_ID}', {
+                  page_path: window.location.pathname,
+                });
+              `}
+            </Script>
+          </>
+        )}
       </head>
       <body className={`${inter.variable} font-sans antialiased`}>
         {children}
